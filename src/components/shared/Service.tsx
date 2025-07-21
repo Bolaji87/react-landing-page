ad37d552ade4f91c4ed74da75058c4f0e6114b31
Bolaji87 <sakabolaji77@gmail.com>
2025-07-21 13:12:26 +0100

    create and add Services section, and Service component

---
+++ src/components/shared/Service.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Paragraph from "./Paragraph";
+interface ServiceProps {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+}
+
+function Service({ title, icon, description }: ServiceProps) {
+  return (
+    <div className="p-5 sm:p-6 lg:p-8 rounded-3xl border border-box-border bg-box-bg shadow-lg shadow-box-shadow relative overflow-hidden">
+      <div className="rounded-xl bg-gray-300 dark:bg-gray-950 p-3 text-heading-1 w-max relative">
+        {icon}
+      </div>
+      <div className="mt-6 space-y-4 relative">
+        <h2 className="text-lg md:text-xl font-semibold">{title}</h2>
+        <Paragraph>{description}</Paragraph>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
